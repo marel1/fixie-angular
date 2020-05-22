407e7f4c0c1deae0e51fa865866db943c77d62e5
Marlena Brzeziok <marel1@autograf.pl>
2020-05-22 13:01:59 +0200

    Create a homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule
