8a055656dd0c2f2d31e882ca8cafd97f806618d4
Marlena Brzeziok <marel1@autograf.pl>
2020-06-01 16:03:44 +0200

    Create a homepage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { MainComponent } from './main/main.component';
+import {Router, RouterModule, Routes} from "@angular/router";
+import { SigninComponent } from './signin/signin.component';
+import { ErrorComponent } from './error/error.component';
+import { SignupComponent } from './signup/signup.component';
 
+const appRoutes:Routes=[
+  {
+    path:'',
+    component:MainComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'SignIn',
+    component:SigninComponent
+  },
+  {
+    path:'SignUp',
+    component:SignupComponent
+  },
+  {
+    path:'**',
+    component:ErrorComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavigationComponent,
-    MainComponent
+    MainComponent,
+    SigninComponent,
+    ErrorComponent,
+    SignupComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,{enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
